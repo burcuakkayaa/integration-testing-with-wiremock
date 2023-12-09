06a9ae5c2d6959d833592d3114f5f3c2462da13d
burcu.akkaya <burcu.akkaya@trendyol.com>
2023-12-09 14:40:01 +0300

    API Base Test class is added

---
+++ src/acceptance-tests/kotlin/api/ApiBaseTest.kt
@@ -0,0 +1,30 @@
+package api
+
+import api.core.constant.Keywords.HOST
+import api.core.constant.Keywords.PORT
+import api.core.constant.Keywords.TR_ZONE_ID
+import api.core.utilitys.LogUtils.Companion.logInfo
+import com.github.tomakehurst.wiremock.client.WireMock
+import org.testng.annotations.BeforeSuite
+import java.time.ZoneId
+import java.util.*
+
+open class ApiBaseTest {
+
+    @BeforeSuite
+    fun setUpSuite() {
+        logInfo("(*) START TIME OF TESTS: " + Date().toInstant().atZone(ZoneId.of(TR_ZONE_ID)))
+        mockServerConfiguration()
+    }
+
+    /**
+     * To Configure Wiremock Server
+     */
+    open fun mockServerConfiguration() {
+        WireMock.configureFor(HOST, PORT)
+
+        WireMock.removeAllMappings()
+        WireMock.resetAllRequests()
+    }
+
+}
--- src/main/kotlin/api/core/constant/Keywords.kt
+++ src/main/kotlin/api/core/constant/Keywords.kt
@@ -3,5 +3,8 @@ package api.core.constant
 object Keywords {
 
     const val FAILED_TEST_RETRY_COUNT = 3
+    const val TR_ZONE_ID = "Europe/Istanbul"
+    const val HOST = "localhost"
+    const val PORT = 8089
 
 }
